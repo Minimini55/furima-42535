006e5823527b8e740957e9a513957bf3bcfc3102
Minimini55 <minami25feb@gmail.com>
2025-09-15 21:52:22 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :ensure_correct_user, only: [:edit, :update]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -22,6 +24,19 @@ class ItemsController < ApplicationController
     @user = @item.user
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
@@ -29,5 +44,15 @@ class ItemsController < ApplicationController
       :image, :name, :description, :category_id, :condition_id, :item_condition_id,
       :shipping_fee_payer_id, :prefecture_id, :days_to_ship_id, :day_to_ship_id, :price
     ).merge(user_id: current_user.id)
+  end
+
+  def ensure_correct_user
+    # URLから編集したい商品の情報を取得する
+    @item = Item.find(params[:id])
+    # ログイン中のユーザーIDと商品の出品者のIDが一致しない場合
+    return if @item.user_id == current_user.id
+
+    # トップページにリダイレクトさせる
+    redirect_to root_path
   end
 end
